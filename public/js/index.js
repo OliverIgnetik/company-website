4da8b0633dee6d32377f2a324c99a42f938c9ed6
Oliver Ignetik <olli@ozemail.com.au>
2019-11-06 14:38:28 +1100

    Added animations and changed layout

--- public/js/index.js
+++ public/js/index.js
@@ -1,43 +1,15 @@
-function onClick(element) {
-    document.getElementById('img01').src = element.src;
-    document.getElementById('modal01').style.display = 'block';
-    var captionText = document.getElementById('caption');
-    captionText.innerHTML = element.alt;
-}
-
 // Toggle between showing and hiding the sidebar when clicking the menu icon
 var mySidebar = document.getElementById('mySidebar');
 
 function w3_open() {
-    if (mySidebar.style.display === 'block') {
-        mySidebar.style.display = 'none';
-    } else {
-        mySidebar.style.display = 'block';
-    }
+  if (mySidebar.style.display === 'block') {
+    mySidebar.style.display = 'none';
+  } else {
+    mySidebar.style.display = 'block';
+  }
 }
 
 // Close the sidebar with the close button
 function w3_close() {
-    mySidebar.style.display = 'none';
-}
-
-// carousel js
-var slideIndex = 0;
-showSlides();
-
-function showSlides() {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";  
-  }
-  slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}    
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " active";
-  setTimeout(showSlides, 3000); // Change image every 5 seconds
+  mySidebar.style.display = 'none';
 }
