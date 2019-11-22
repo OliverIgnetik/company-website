447625cd6e16184bbc82e47e1ca3cc283c606d69
Oliver Ignetik <olli@ozemail.com.au>
2019-11-22 15:10:56 +1100

    22-11-19 preloader added

--- public/js/index.js
+++ public/js/index.js
@@ -1,3 +1,14 @@
+// website loader
+$(document).ready(function () {
+  // listen for load of window
+  setTimeout(function () {
+    $(".loader-wrapper").addClass('zero-opacity')
+  }, 2000);
+  setTimeout(function () {
+    $(".loader-wrapper").addClass('hidden');
+  }, 3000);
+});
+
 // Toggle between showing and hiding the sidebar when clicking the menu icon
 var mySidebar = document.getElementById('mySidebar');
 
@@ -13,16 +24,3 @@ function w3_open() {
 function w3_close() {
   mySidebar.style.display = 'none';
 }
-
-// website loader
-$(document).ready(function () {
-  $(window).on("load", () => {
-    // listen for load of window
-    setTimeout(function () {
-      $(".loader-wrapper").addClass('zero-opacity')
-    }, 2000);
-    setTimeout(function () {
-      $(".loader-wrapper").addClass('hidden');
-    }, 3000);
-  })
-});
